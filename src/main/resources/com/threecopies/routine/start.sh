c7d771bd58eb8705246c654a7e331bab514349d6
Yegor Bugayenko <yegor256@gmail.com>
2017-09-06 18:32:05 +0300

    #1 docker pull

--- src/main/resources/com/threecopies/routine/start.sh
+++ src/main/resources/com/threecopies/routine/start.sh
@@ -2,6 +2,7 @@
 
 container=$1
 period=$2
+image=yegor256/threecopies
 
 if docker ps | grep --quiet "\s${container}\s*\$"; then
   echo "Docker container ${container} is still running"
@@ -11,11 +12,12 @@ fi
 cd "$(dirname "$0")"
 cd "${container}"
 chmod a+x script.sh
+docker pull "${image}"
 docker run -t --rm --name "${container}" \
     -v "$(pwd):/main" \
     --hostname=docker --privileged \
     --memory=6g --memory-swap=16g --oom-kill-disable \
     -e="period=${period}" \
     -w="/root" \
-    yegor256/threecopies /main/script.sh \
+    "${image}" /main/script.sh \
     > log 2>&1; echo $? > exit
